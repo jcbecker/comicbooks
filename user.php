a6fd4348222ead7c83333080e7db72857a5aef66
jcbecker <joaoc.becker@hotmail.com>
2015-11-24 02:35:08 -0200

    Mostra se quem ta logado é adm ou leitor

--- user.php
+++ user.php
@@ -1,7 +1,29 @@
 <?php 
 include_once "protect.php";
 protect();
+if(adminlogado()){
+    $conta="admin";
+}else{
+    $conta="leitor";
+}
 ?>
+<!doctype html>
+
 <html>
-oi
+<head>
+    <title>comicbooks | User</title>
+    <meta charset = "utf-8"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+<body>
+    <div id="main">
+        <?php include_once "conteudo/header.php"; ?>
+        <div id="content">
+            <?php echo $conta; ?>
+        </div>
+        
+        
+        <?php include_once "conteudo/footer.php"; ?>
+    </div>
+</body>
 </html>
