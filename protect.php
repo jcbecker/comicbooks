ae3f6b1f1e73874aadcff76d5231938174006947
jcbecker <joaoc.becker@hotmail.com>
2015-11-24 01:49:27 -0200

    criada a pagina de funções protect, gerencia os niveis de acesso de cada usuario

---
+++ protect.php
@@ -0,0 +1,26 @@
+<?php 
+if (!function_exists("protect")){
+    function protect(){//tenq incluir protect.php e chamar a função protect
+        if (!isset($_SESSION)){
+            session_start();
+        }
+        if (!isset($_SESSION['user']) || strlen($_SESSION['user'])<6 || strlen($_SESSION['user'])>30){
+            header("Location:index.php");
+            
+        }
+    }
+}
+if (!function_exists("adminprotect")){
+    function adminprotect(){//
+        if (!isset($_SESSION)){
+            session_start();
+        }
+        if (!isset($_SESSION['user']) || strlen($_SESSION['user'])<6 || strlen($_SESSION['user'])>30 || $_SESSION['nivel']!=2){//nivel 2 vai ser o nivel de um adm, pq sim
+            header("Location:index.php");
+            
+        }
+    }
+}
+
+
+?>
