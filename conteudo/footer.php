8d52b70fa047bff28c7c48cf848e24d4780398c9
jcbecker <joaoc.becker@hotmail.com>
2015-11-25 03:11:11 -0200

    agr tem link do github no rodapé

--- conteudo/footer.php
+++ conteudo/footer.php
@@ -1,3 +1,4 @@
 <footer>
     <p>ComicBooks By: Cleiton Ambrosini e João Becker</p>
+    <p>Repositório deste site no <a href="https://github.com/jcbecker/comicbooks" target="_blank">GitHub</a><br></p>
 </footer>
